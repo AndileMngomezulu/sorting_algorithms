5ef91cc7894fcc8e53db98f1535f24202e9c04fe
AndileMngomezulu <andymdlalose1@gmail.com>
2024-01-24 07:25:26 -0800

    Selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -27,7 +27,7 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size - 1;; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = array + 1;
 		for (j = i + 1; j < size; j++)
